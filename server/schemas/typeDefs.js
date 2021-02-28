813c67fffa0fc9fe96052a0f8933bb98d5d44424
Calista Mayer <calista.a.mayer@gmail.com>
2021-02-27 17:38:06 -0800

    auth middleware and mutations

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -12,21 +12,29 @@ const typeDefs = gql`
     }
 
     type Book {
-        // bookId: not the _id but the book's id value returned from Google's Book API
-        authors: [String]
+        bookId: String!
+        authors: [String!]
         description: String
-        title: String
+        title: String!
         image: String
         link: String
     }
 
     type Auth {
-        token: String
+        token: ID!
         user: User
     }
 
     type Query {
         me: User
+        user(username: String!): User
+    }
+
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        saveBook(title: String!, author: [String!], description: String!, bookId: String!, image: String, link: String): User
+        removeBook(bookId: String!): User
     }
 `;
 
--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const db = require('./config/connection');
 const routes = require('./routes');
 // import ApolloServer
 const { ApolloServer } = require('apollo-server-express');
+const { authMiddleware } = require('./utils/auth');
 
 // import typeDefs and resolvers
 const { typeDefs, resolvers } = require('./schemas');
@@ -14,7 +15,8 @@ const PORT = process.env.PORT || 3001;
 // create a new Apollo server and pass in schema data
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  context: authMiddleware
 });
 
 // integrate Apollo server with Express application as middleware
