f3e63495702d899d79438d93a69c013e2e7fd625
Calista Mayer <calista.a.mayer@gmail.com>
2021-03-01 20:30:29 -0800

    login and signup mutations

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,8 +1,28 @@
 import gql from 'graphql-tag';
 
-export const LOGIN_USER = qgl``;
+export const LOGIN_USER = qgl`
+    mutation login($email: String!, $password: String!) {
+        login(email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+            }
+        }
+    }
+`;
 
-export const ADD_USER = gql``;
+export const ADD_USER = gql`
+    mutation addUser($username: String!, $email: String!, $password: String!) {
+        addUser(username: $username, email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+            }
+        }
+    }
+`;
 
 export const SAVE_BOOK = gql``;
 
